d48fc60c95d0e265eb41b91dacd2baed08110e9f
achyut vardhan <achyutvardhan234@gmail.com>
2022-08-27 01:22:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,31 @@ if (found) {
 }
 })
 
+app.put(('/edit/:pnr'),(req,res)=>{
+    const found = dbdata.some(member=>member.pnr === req.params.pnr)
+if (found) {
+    const updUser = req.body;
+    dbdata.forEach(element=>{
+        if (element.pnr === req.params.pnr) {
+            element.user_name =updUser.user_name?updUser.user_name : element.user_name;
+            element.seat_number =updUser.seat_number?updUser.seat_number : element.seat_number;
+            element.birth_number =updUser.birth_number?updUser.birth_number : element.birth_number;
+            element.from =updUser.from?updUser.from : element.from;
+            element.to =updUser.to?updUser.to : element.to;
+            element.date_of_departure =updUser.date_of_departure?updUser.date_of_departure : element.date_of_departure;
+            element.time_of_arrival =updUser.time_of_arrival?updUser.time_of_arrival : element.time_of_arrival;
+            element.date_of_arrival =updUser.date_of_arrival?updUser.date_of_arrival : element.date_of_arrival;
+            element.time_of_departure =updUser.time_of_departure?updUser.time_of_departure : element.time_of_departure;
+            element.pnr =updUser.pnr?updUser.pnr : element.pnr;
+res.json({msg: updated,element})
+            
+        }
+    })
+   
+}else{
+    res.status(400).send('Data NOT FOUND TO BE EDIT')
+}
+})
 app.get(('*'),(req,res)=>{
 res.status(404).send('fuckk off')
 })
