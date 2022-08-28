e91b68528c08b106f1be1747bb3d9be5efdc989b
achyut vardhan <achyutvardhan234@gmail.com>
2022-08-29 01:32:32 +0530

    installed mysql

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+// const mysql = require("mysql");
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
+
 app.use(require("./crypto"));
+
 app.use("/userMember", require("./routes/api/userMember"));
 
 app.listen(5000, () => {
