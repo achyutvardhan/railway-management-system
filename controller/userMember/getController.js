2b70c2f567ace3f5b66c00efad74b46bf08033aa
achyut vardhan <achyutvardhan234@gmail.com>
2022-08-30 02:29:32 +0530

    updated getcontroller

--- controller/userMember/getController.js
+++ controller/userMember/getController.js
@@ -1,11 +1,14 @@
 const dbdata = require("../../db");
-
+const con = require("../../database/mysql");
 function get(req, res) {
-  const found = dbdata.some((member) => member.pnr === req.params.pnr);
-  if (found) {
-    res.json(dbdata.filter((member) => member.pnr === req.params.pnr));
-  } else {
-    res.status(400).send("Data NOT FOUND");
+  let pnr = req.params.pnr;
+
+  //fetch from database
+  async function getData() {
+    var sql = `select * from railwayManagment where pnr = '${pnr}'`;
+    var result = await con.query(sql);
+    res.json(result[0][0]);
   }
+  getData();
 }
 module.exports.get = get;
